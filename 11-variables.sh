5306874e8a4b083f9f2b3758eacf991497c303d4
Jagadeesh-Vardhineni <jagadeesh.dev@gmail.com>
2026-07-01 12:30:37 +0530

    Variables updated

---
+++ 11-variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+USERID= $(id -u)
+
+if[USERID -ne 0];then
+  echo"Please run the file with Sudo access"
+  exit 1
+fi
+
+validate(){
+    if [$1 -ne 0] then
+      echo "$2 .... Failure"
+    else 
+       echo "$1....Sucess"
+   fi
+}
+
+dnf intsall ngnix -y
+validate $? "Installing Nginx"
+
+dnf install  mysql -y
+validate $? "Installing Mysql"
